ddcacbc6bcb7b59202c0902448bf5af8e5f4cdbc
Fuzail Shams Mullappilly <fuzailshams70@gmail.com>
2024-04-09 01:44:22 +0530

    Added new files

--- bank/bank.py
+++ bank/bank.py
@@ -3,8 +3,8 @@
 # If the greeting starts with an “h” (but not “hello”), output $20.
 # Otherwise, output $100. Ignore any leading whitespace in the user’s greeting,
 # and treat the user’s greeting case-insensitively
-great = input("Greetings: ")
-greating=str.lower(str.strip(great))
+
+greating=str.lower(str.strip(input("Greetings: ")))
 if greating.startswith("hello"):
     print("$0")
 elif greating[0] == "h":
---
+++ extensions/extensions.py
@@ -0,0 +1,19 @@
+ext=str.strip(str.lower(input("File name: ")))
+if ext.endswith(".gif"):
+    print("image/gif")
+elif ext.endswith(".jpg"):
+    print("image/jpeg")
+elif ext.endswith(".jpeg"):
+    print("image/jpeg")
+elif ext.endswith(".png"):
+    print("image/png")
+elif ext.endswith(".pdf"):
+    print("application/pdf")
+elif ext.endswith(".txt"):
+    print("text/plain")
+elif ext.endswith(".zip"):
+    print("application/zip")
+elif ext.endswith(".bin"):
+    print("application/octet-stream")
+elif ext.endswith(""):
+    print("application/octet-stream")
