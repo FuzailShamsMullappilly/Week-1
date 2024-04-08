f39760d70737785bf1a5f6a8ed83ba8d44751512
Fuzail Shams Mullappilly <fuzailshams70@gmail.com>
2024-04-09 01:02:57 +0530

    Uploading

---
+++ bank/bank.py
@@ -0,0 +1,13 @@
+#This program greets the user and 
+# If the greeting starts with “hello”, output $0.
+# If the greeting starts with an “h” (but not “hello”), output $20.
+# Otherwise, output $100. Ignore any leading whitespace in the user’s greeting,
+# and treat the user’s greeting case-insensitively
+great = input("Greetings: ")
+greating=str.lower(str.strip(great))
+if greating.startswith("hello"):
+    print("$0")
+elif greating[0] == "h":
+    print("$20")
+elif greating[0] != "h":
+    print("$100")
